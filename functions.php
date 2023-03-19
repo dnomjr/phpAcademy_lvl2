368f1c2875a89e3cc3673df4e5246b25669abe30
dnomjr <120389887+dnomjr@users.noreply.github.com>
2023-03-19 18:09:21 +0100

    Add files via upload

--- functions.php
+++ functions.php
@@ -27,7 +27,6 @@ function mkfile( $filename )
         return false;
 }
 
-
 /**
  * Ukladaj aktualny datum a cas do suboru (ak uz v subore existuje datum a cas, novy cas sa pripise), kazdy zaznam daj na novy riadok
  *
@@ -71,4 +70,104 @@ function read_data($filename)
     return file_get_contents( $filename );
 }
 
+
+/**
+* funkcie, ktore suvisia s logovanim studenti.json, prerob do classy na staticke funkcie
+* sprav studenti.json, kde sa budu ukladat studenti ktori prisli
+* ak subor existuje tak loadni stary studenti.json, a pridaj novy zaznam
+* incrementuj pri prichode studenta cislo v jsone, ktore bude reprezentovat     celkovy pocet prichodov
+ */
+
+ class Studenti
+ {
+    public static function addStudent($filename, $newStudent)
+    {    
+        if(is_file($filename))
+        {
+            $arrayStudents = json_decode(file_get_contents($filename), true);
+        }
+    
+        if(empty($arrayStudents))
+        {
+            $arrayStudents = [
+                'pocetPrichodov' => 0
+            ];
+        }
+        
+        array_push($arrayStudents, $newStudent);
+        $arrayStudents['pocetPrichodov']++;
+        
+        file_put_contents( $filename, json_encode($arrayStudents), true);
+        echo "<hr>";
+        echo "Celkovy počet príchodov je " . $arrayStudents['pocetPrichodov'].".";
+    } 
+ }
+
+/**
+ * vypis obsah mapy studentov po decodovani pomocou print_r 
+ */
+function loadJson($filename)
+{
+    echo "<hr>";
+    return json_decode(file_get_contents($filename), true);
+}
+
+
+/**
+ * funkcie, ktore suvisia s logovanim prichody.json, prerob do classy a funkcie pouzivaj tak ze najprv vytvoris instanciu classy (objekt)
+ * 
+ * sprav prichody.json, ktory bude len pole vsetkych prichodov, a rovnako ho appenduj decodovanim a encodovanim jsonu
+ * preiteruj pole z prichody.json, a k meskajucim datumom napis ze ""meskanie""
+ */
+
+Class Prichody
+{
+    public $file;
+    public $Date;
+    public $Late;
+
+    function addDate($filename, $arriveDate, $tooLate)
+    {
+    $this->file = $filename;
+    $this->Date = $arriveDate;
+    $this->Late = $tooLate;
+
+    if(is_file($this->file))
+    {
+        $arrayArrive = json_decode(file_get_contents($this->file));
+    }
+    
+    if(empty($arrayArrive))
+    {
+        $arrayArrive = [];
+    }
+    
+    // check for late arrival - w/ private method
+    $isLate = $this->checkLate($this->Late, $this->Date);
+    if ($isLate) {
+        $this->Date = date('d.m.Y, H:i:s') . " meskanie";
+    }
+
+    array_push($arrayArrive, $this->Date);
+    file_put_contents($this->file, json_encode($arrayArrive));
+    }
+
+    /**
+     * v classe ktoru pouzivas ako instanciu, vytvor private metodu ktoru pouzijes ako nejaku pomocnu feature pri logovani (napriklad ziskavanie ci nastalo meskanie)
+     */
+    private function checkLate($tooLate, $arriveDate)
+    {
+        $this->Late = $tooLate;
+        $this->Date = $arriveDate;
+
+        if ($this->Late == $this->Date)
+        {
+            return true;
+        } 
+        else
+        {
+            return false;
+        }
+    }
+}
 ?>
--- index.php
+++ index.php
@@ -7,134 +7,62 @@
     <title>Level2</title>
 </head>
 <body>
+
 <?php include "functions.php" ?> 
-
-
 <?php 
     date_default_timezone_set('Europe/Bratislava');
     $date = actual_date(); //actual date & time
-    
     echo "Actual date and time is ".$date; 
+?>
 
+    <form action="" method="get">
+    <label for="name">Your first & last name: </label>
+    <input type="text" name="name">
+    <input type="submit" value="Submit!" name="submit">
+    </form>
+
+<?php
 /**
-* pridaj form s inputom meno studenta, ktory sa bude submitovat cez POST - OK
-
-* dorob aby sa pri prichode logovalo aj meno studenta - OK
-
+* dorob aby sa pri prichode logovalo aj meno studenta
 * sprav aby to akceptovalo meno aj cez url adresu ze ?meno=jozko
 */
-
     $file = 'log.txt';
+    mkfile($file); // create new file for login arrivals
 
-    mkfile('log.txt'); // create new file for logging arrivals
-    /* form w/ student name & submit set on GET*/
-    echo '<form action="" method="get">
-          <label for="name">Your first & last name: </label>
-          <input type="text" name="name">
-          <input type="submit" value="Submit!" name="submit">
-          </form>';
-
-          /*arrivals*/
+    /*arrivals*/
     if (isset($_GET['name']) ) 
     {
-        $their_name = $_GET['name']; //student name
+        $studentName = $_GET['name']; //student name variable
 
         $delay = (date('H:i:s') > date('08:00:00') && date('H:i:s') < date('20:00:00'));
         $ontime = (date('H:i:s') >= date('00:00:00') && date('H:i:s') <= date('08:00:00'));
 
-        write_data($file, $date, $their_name, $delay, $ontime); // put data to exist file
+        write_data($file, $date, $studentName, $delay, $ontime); // put data to exist file
     }
     
     echo nl2br(read_data( $file )); // get data from log.txt to index.php
 
+    /** STUDENTI.JSON */
+    $jsonStudents = 'studenti.json';
+    mkfile($jsonStudents);    // create new json file for student names
 
-   
-/**
- * sprav studenti.json, kde sa budu ukladat studenti ktori prisli - OK
-
-* ak subor existuje tak loadni stary studenti.json, a pridaj novy zaznam - OK
-
-* incrementuj pri prichode studenta cislo v jsone, ktore bude reprezentovat celkovy pocet prichodov - OK
-
-* vypis obsah mapy studentov po decodovani pomocou print_r - OK
- */
-
-    $json_students = 'studenti.json'; // create new json file for student names
-    if(is_file($json_students)) //if json file exist
+    if(isset($studentName))
     {
-        $students = json_decode(read_data($json_students), true); //save to $students json file content and load old file
-    
-        if(empty($students))   //if array is empty, then create new array w/ arrivals
-        {
-            $students = [
-                'arrivals' => 0
-            ] ;
-        }
-
-        if(isset($_GET['name']))
-        {
-        array_push($students, $_GET['name']); //push student names to array
-        $students['arrivals']++; // increment number of arrivals when student come
-        }
+        echo(Studenti::addStudent($jsonStudents, $studentName));
     }
-    
-    file_put_contents($json_students, json_encode($students), true); //save students and number of arrivals to json file
-    echo "<hr>";
-
-    print_r($students); //write content of json file after decode
-
-    echo "<hr>";
-
-    echo "Celkový počet príchodov študentov je " . $students['arrivals'].".";
-
-    echo "<hr>";
+    print_r(loadJson($jsonStudents));
 
 
-/**
- * sprav prichody.json, ktory bude len pole vsetkych prichodov, a rovnako ho appenduj decodovanim a encodovanim jsonu
- */
-    $json_arrivals = 'prichody.json';
-    if(is_file($json_arrivals))
-    {
-        $arrivals = json_decode(read_data($json_arrivals));
-    }
-    else
-    {
-        $arrivals = [];
-    }
-    
-    if(empty($arrivals)) 
-    {
-        $arrivals = [];
-    }
-    
-    /**
-     * preiteruj pole z prichody.json, a k meskajucim datumom napis ze ""meskanie""
-     */
+    /** PRICHODY.JSON */
+    $jsonArrivals = 'prichody.json';
+    mkfile($jsonArrivals);
+
     if(isset($_GET['submit']))
     {
-        if ($delay == $date)
-        {
-        $date = actual_date() . " meskanie";
-        }
-
-        else
-        {
-            foreach($arrivals as $late_arrivals) 
-            {
-                if($delay == $late_arrivals)
-                {
-                    $date = actual_date() . " meskanie"; 
-                }
-            }    
-        }
-        array_push($arrivals, $date);
+        $arrive = new Prichody();
+        $arrive->addDate($jsonArrivals, $date, $delay);
     }
-
-    file_put_contents($json_arrivals, json_encode($arrivals));
-    print_r($arrivals);
+    print_r(loadJson($jsonArrivals));
 ?>
-
-
 </body>
 </html>
