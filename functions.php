3104318f87e4240b5deee98092e5e7cc747d2f12
dnomjr <120389887+dnomjr@users.noreply.github.com>
2023-03-24 16:34:57 +0100

    Add files via upload

--- functions.php
+++ functions.php
@@ -1,173 +1,157 @@
 <?php
-
-/**
- * Vypis aktualny datum a cas naformatovany
- *
- * @return string
- */
-function actual_date()
-{
-    return date('d.m.Y, H:i:s');
-}
-
-
-/**
- * Vytvori novy subor, ak uz existuje tak vrati false.
- *
- * @param string $filename
- * @return boolean
- */
-function mkfile( $filename )
-{
-    if ( ! is_file( $filename ))
+    /**
+     * Vypis aktualny datum a cas naformatovany
+     *
+     * @return string
+     */
+    function actual_date()
     {
-        fclose(fopen( $filename, 'x'));
-        return true;
-    }
-        return false;
-}
-
-/**
- * Ukladaj aktualny datum a cas do suboru (ak uz v subore existuje datum a cas, novy cas sa pripise), kazdy zaznam daj na novy riadok
- *
- * Ak prisiel student po 8:00, tak dopis do logu za cas string "meskanie"
- * 
- * Ak pride student medzi 20-24, tak vyhod chybu cez die, ze nemoze sa dany prichod zapisat
- * 
- * Dorob aby sa pri prichode logovalo aj meno studenta
- * 
- * @param string $log_file
- * @param string $time
- * @return string
- */
-function write_data($filename, $time, $name, $late, $welcome)
-{
-    if ($late)
-    {
-        return file_put_contents($filename, "$time || Hello $name. You are too late!" . PHP_EOL, FILE_APPEND); 
-    }
-
-    elseif ($welcome)
-    {
-        return file_put_contents($filename, "$time || Hello $name. You are welcome!" . PHP_EOL, FILE_APPEND);
-    }
-
-    else
-    {
-        die ("Hello $name. You are too late! Door between 20:00 and 23:59 are closed");
-    } 
-}
-    
-
-/**
- * Getuj obsah log suboru a vypis ho
- *
- * @param string $log_file
- * @return string
- */
-function read_data($filename)
-{
-    return file_get_contents( $filename );
-}
-
-
-/**
-* funkcie, ktore suvisia s logovanim studenti.json, prerob do classy na staticke funkcie
-* sprav studenti.json, kde sa budu ukladat studenti ktori prisli
-* ak subor existuje tak loadni stary studenti.json, a pridaj novy zaznam
-* incrementuj pri prichode studenta cislo v jsone, ktore bude reprezentovat     celkovy pocet prichodov
- */
-
- class Studenti
- {
-    public static function addStudent($filename, $newStudent)
-    {    
-        if(is_file($filename))
-        {
-            $arrayStudents = json_decode(file_get_contents($filename), true);
-        }
-    
-        if(empty($arrayStudents))
-        {
-            $arrayStudents = [
-                'pocetPrichodov' => 0
-            ];
-        }
-        
-        array_push($arrayStudents, $newStudent);
-        $arrayStudents['pocetPrichodov']++;
-        
-        file_put_contents( $filename, json_encode($arrayStudents), true);
-        echo "<hr>";
-        echo "Celkovy počet príchodov je " . $arrayStudents['pocetPrichodov'].".";
-    } 
- }
-
-/**
- * vypis obsah mapy studentov po decodovani pomocou print_r 
- */
-function loadJson($filename)
-{
-    echo "<hr>";
-    return json_decode(file_get_contents($filename), true);
-}
-
-
-/**
- * funkcie, ktore suvisia s logovanim prichody.json, prerob do classy a funkcie pouzivaj tak ze najprv vytvoris instanciu classy (objekt)
- * 
- * sprav prichody.json, ktory bude len pole vsetkych prichodov, a rovnako ho appenduj decodovanim a encodovanim jsonu
- * preiteruj pole z prichody.json, a k meskajucim datumom napis ze ""meskanie""
- */
-
-Class Prichody
-{
-    public $file;
-    public $Date;
-    public $Late;
-
-    function addDate($filename, $arriveDate, $tooLate)
-    {
-    $this->file = $filename;
-    $this->Date = $arriveDate;
-    $this->Late = $tooLate;
-
-    if(is_file($this->file))
-    {
-        $arrayArrive = json_decode(file_get_contents($this->file));
-    }
-    
-    if(empty($arrayArrive))
-    {
-        $arrayArrive = [];
-    }
-    
-    // check for late arrival - w/ private method
-    $isLate = $this->checkLate($this->Late, $this->Date);
-    if ($isLate) {
-        $this->Date = date('d.m.Y, H:i:s') . " meskanie";
-    }
-
-    array_push($arrayArrive, $this->Date);
-    file_put_contents($this->file, json_encode($arrayArrive));
+        return date('d.m.Y, H:i:s');
     }
 
     /**
-     * v classe ktoru pouzivas ako instanciu, vytvor private metodu ktoru pouzijes ako nejaku pomocnu feature pri logovani (napriklad ziskavanie ci nastalo meskanie)
+     * Vytvori novy subor, ak uz existuje tak vrati false.
+     *
+     * @param string $filename
+     * @return boolean
      */
-    private function checkLate($tooLate, $arriveDate)
-    {
-        $this->Late = $tooLate;
-        $this->Date = $arriveDate;
+    function mkfile( $filename )
+    {     
+        fopen( $filename, 'c');    
+    }
 
-        if ($this->Late == $this->Date)
+    /**
+     * Ukladaj aktualny datum a cas do suboru (ak uz v subore existuje datum a cas, novy cas sa pripise), kazdy zaznam daj na novy riadok
+     *
+     * Ak prisiel student po 8:00, tak dopis do logu za cas string "meskanie"
+     * 
+     * Ak pride student medzi 20-24, tak vyhod chybu cez die, ze nemoze sa dany prichod zapisat
+     * 
+     * Dorob aby sa pri prichode logovalo aj meno studenta
+     * 
+     * @param string $log_file
+     * @param string $time
+     * @return string
+     */
+    function write_data($mkFile, $filename, $time, $name, $late, $welcome)
+    {
+        if ($late)
         {
-            return true;
-        } 
+            return file_put_contents($filename, "$time || Hello $name. You are too late!" . PHP_EOL, FILE_APPEND); 
+        }
+        elseif ($welcome)
+        {
+            return file_put_contents($filename, "$time || Hello $name. You are welcome!" . PHP_EOL, FILE_APPEND);
+        }
         else
         {
-            return false;
+            die ("Hello $name. You are too late! Door between 20:00 and 23:59 are closed");
+        } 
+    }   
+
+    /**
+     * Getuj obsah log suboru a vypis ho
+     *
+     * @param string $log_file
+     * @return string
+     */
+    function read_data($filename)
+    {
+        if (file_exists($filename) && pathinfo($filename, PATHINFO_EXTENSION) === 'json')
+        { 
+            return json_decode(file_get_contents($filename), true);
+        }
+       else 
+       {
+           return file_get_contents($filename);
+       }
+    }
+     
+    /**
+    * funkcie, ktore suvisia s logovanim studenti.json, prerob do classy na staticke funkcie
+    * sprav studenti.json, kde sa budu ukladat studenti ktori prisli
+    * ak subor existuje tak loadni stary studenti.json, a pridaj novy zaznam
+    * incrementuj pri prichode studenta cislo v jsone, ktore bude reprezentovat     celkovy pocet prichodov
+    */
+
+    class Students
+    {
+        public static function addStudent($filename, $newStudent)
+        {    
+            if(is_file($filename))
+            {
+                $students = json_decode(file_get_contents($filename), true);
+            }
+
+            if(empty($students))
+            {
+                $students = [
+                    'pocetPrichodov' => 0
+                ];
+            }
+            
+            array_push($students, $newStudent);
+            $students['pocetPrichodov']++;
+            
+            file_put_contents( $filename, json_encode($students));
+            echo "<hr>";
+            echo "Celkovy počet príchodov je " . $students['pocetPrichodov'].".";
+            echo "<hr>";
+        } 
+    }
+
+    /**
+     * funkcie, ktore suvisia s logovanim prichody.json, prerob do classy a funkcie pouzivaj tak ze najprv vytvoris instanciu classy (objekt)
+     * 
+     * sprav prichody.json, ktory bude len pole vsetkych prichodov, a rovnako ho appenduj decodovanim a encodovanim jsonu
+     * preiteruj pole z prichody.json, a k meskajucim datumom napis ze ""meskanie""
+     */
+    Class Arrivals
+    {
+        public $file;
+        public $Date;
+        public $Late;
+
+        function addDate($filename, $arriveDate, $tooLate)
+        {
+            $this->file = $filename;
+            $this->Date = $arriveDate;
+            $this->Late = $tooLate;
+
+            if(is_file($this->file))
+            {
+                $arrive = json_decode(file_get_contents($this->file));
+            }
+            
+            if(empty($arrive))
+            {
+                $arrive = [];
+            }
+            
+            // check for late arrival - w/ private method
+            $isLate = $this->checkLate();
+            if ($isLate)
+            {
+                $this->Date = date('d.m.Y, H:i:s') . " meskanie";
+            }
+
+            array_push($arrive, $this->Date);
+            file_put_contents($this->file, json_encode($arrive));
+        }
+
+        /**
+         * v classe ktoru pouzivas ako instanciu, vytvor private metodu ktoru pouzijes ako nejaku pomocnu feature pri logovani (napriklad ziskavanie ci nastalo meskanie)
+         */
+        private function checkLate()
+        {
+            if ($this->Late == $this->Date)
+            {
+                return true;
+            } 
+            else
+            {
+                return false;
+            }
         }
     }
-}
 ?>
--- index.php
+++ index.php
@@ -1,68 +1,71 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Level2</title>
-</head>
-<body>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Level2</title>
+    </head>
 
-<?php include "functions.php" ?> 
-<?php 
-    date_default_timezone_set('Europe/Bratislava');
-    $date = actual_date(); //actual date & time
-    echo "Actual date and time is ".$date; 
-?>
+    <body>
+        <?php include "functions.php" ?> 
+        <?php 
+            date_default_timezone_set('Europe/Bratislava');
+            $date = actual_date(); //actual date & time
+            echo "Actual date and time is ".$date; 
+        ?>
 
-    <form action="" method="get">
-    <label for="name">Your first & last name: </label>
-    <input type="text" name="name">
-    <input type="submit" value="Submit!" name="submit">
-    </form>
+        <form action="" method="get">
+            <label for="name">Your first & last name: </label>
+            <input type="text" name="name">
+            <input type="submit" value="Submit!" name="submit">
+        </form>    
 
-<?php
-/**
-* dorob aby sa pri prichode logovalo aj meno studenta
-* sprav aby to akceptovalo meno aj cez url adresu ze ?meno=jozko
-*/
-    $file = 'log.txt';
-    mkfile($file); // create new file for login arrivals
+        <?php
+        /**
+        * dorob aby sa pri prichode logovalo aj meno studenta
+        * sprav aby to akceptovalo meno aj cez url adresu ze ?meno=jozko
+        */
+            $file = 'log.txt';
 
-    /*arrivals*/
-    if (isset($_GET['name']) ) 
-    {
-        $studentName = $_GET['name']; //student name variable
+            /*arrivals*/
+            if (isset($_GET['name'])) 
+            {
+                $studentName = $_GET['name']; //student name variable
 
-        $delay = (date('H:i:s') > date('08:00:00') && date('H:i:s') < date('20:00:00'));
-        $ontime = (date('H:i:s') >= date('00:00:00') && date('H:i:s') <= date('08:00:00'));
+                $delay = (date('H:i:s') > date('08:00:00') && date('H:i:s') < date('23:00:00'));
+                $ontime = (date('H:i:s') >= date('00:00:00') && date('H:i:s') <= date('08:00:00'));
 
-        write_data($file, $date, $studentName, $delay, $ontime); // put data to exist file
-    }
-    
-    echo nl2br(read_data( $file )); // get data from log.txt to index.php
+                write_data(mkfile($file), $file, $date, $studentName, $delay, $ontime); // put data to exist file
+            }
+            
+            if(is_file($file))
+            {
+                echo nl2br(read_data( $file )); // get data from log.txt to index.php
+            }
 
-    /** STUDENTI.JSON */
-    $jsonStudents = 'studenti.json';
-    mkfile($jsonStudents);    // create new json file for student names
+            /** STUDENTI.JSON */
+            $jsonStudents = 'studenti.json';
 
-    if(isset($studentName))
-    {
-        echo(Studenti::addStudent($jsonStudents, $studentName));
-    }
-    print_r(loadJson($jsonStudents));
+            if(isset($studentName))
+            {
+                mkfile($jsonStudents);
+                echo(Students::addStudent($jsonStudents, $studentName));
+                print_r(read_data($jsonStudents));
+            }
 
+            echo '<hr>';
 
-    /** PRICHODY.JSON */
-    $jsonArrivals = 'prichody.json';
-    mkfile($jsonArrivals);
-
-    if(isset($_GET['submit']))
-    {
-        $arrive = new Prichody();
-        $arrive->addDate($jsonArrivals, $date, $delay);
-    }
-    print_r(loadJson($jsonArrivals));
-?>
-</body>
+            /** PRICHODY.JSON */
+            $jsonArrivals = 'prichody.json';
+            
+            if(isset($_GET['submit']))
+            {
+                mkfile($jsonArrivals);
+                $arrive = new Arrivals();
+                $arrive->addDate($jsonArrivals, $date, $delay);
+                print_r(read_data($jsonArrivals));
+            }
+        ?>
+    </body>
 </html>
